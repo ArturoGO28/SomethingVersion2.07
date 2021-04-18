c87f7af46eace114d072b30aea2fbfbc02fc6c68
Fernand-Francur <81713670+Fernand-Francur@users.noreply.github.com>
2021-04-18 19:45:15 -0400

    Add files via upload

---
+++ Timo's/V4/TileMap.cpp
@@ -0,0 +1,28 @@
+#include "libs.h"
+#include "TileMap.h"
+
+TileMap::TileMap()
+{
+  //Initialize tilemap
+
+}
+
+TileMap::~TileMap()
+{
+}
+
+void TileMap::addTile(unsigned x, unsigned y)
+{
+}
+
+void TileMap::removeTile(unsigned x, unsigned y)
+{
+}
+
+void TileMap::update()
+{
+}
+
+void TileMap::render()
+{
+}
---
+++ Timo's/V4/TileMap.h
@@ -0,0 +1,23 @@
+#ifndef TILEMAP_H
+#define TILEMAP_H
+
+#include"Tile.h"
+
+class TileMap
+{
+private:
+  std::vector< std::vector<Tile*> > tiles;
+
+public:
+  TileMap();
+  ~TileMap();
+
+  //Functions
+  void addTile(unsigned x, unsigned y);
+  void removeTile(unsigned x, unsigned y);
+
+  void update();
+  void render();
+};
+
+#endif
