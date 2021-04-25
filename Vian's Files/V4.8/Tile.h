2de1dd5e9611e9e53fb2f99362589bf18e03a7fe
vianabar <81713417+vianabar@users.noreply.github.com>
2021-04-24 22:03:03 -0400

    Add files via upload

---
+++ Vian's Files/V4.8/Tile.h
@@ -0,0 +1,25 @@
+#ifndef TILE_H
+#define TILE_H
+#include "libs.h"
+
+class Tile {
+
+private:
+  sf::Clock animationTimer; // Timer for idle animation
+  std::vector<sf::RectangleShape> tiles; //Gridmap
+  std::vector<int> tile_is_space;
+
+public:
+  Tile();
+  virtual ~Tile();
+
+  //Modifiers
+  void setPosition(const float x, const float y);
+  void updateTilemap();
+  const std::vector<sf::RectangleShape> get_Tiles() const;
+  const std::vector<int> get_Tile_is_space() const;
+  void renderTilemap(sf::RenderTarget & target);
+  void initVariables();
+};
+
+#endif
